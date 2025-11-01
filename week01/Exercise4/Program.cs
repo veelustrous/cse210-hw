22351762a9f85fddc170ccaf2f41d14a8d2f3954
veelustrous <vmalagu@byupathway.edu>
2025-11-01 22:39:36 +0100

    latest update to commit

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Globalization;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Welcome, enter a number. [Enter 0 to quit] ");
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int highest = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > highest)
+            {
+                highest = number;
+            }
+        }
+        
+            
+
+        Console.WriteLine($"The highest is: {highest}");
+        
+
+        
     }
 }
